20c7091b72f53e058da4123f03878ce9cbd5d153
MarziaJalili <astronomyfancy@gmail.com>
2025-01-04 19:39:42 +0430

    did stuff

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,19 +1,22 @@
 import { useState } from "react"
 
 function Input(props) {
-    const [error, setError] = useState(props.error);
-
     const errorStyles = {
         borderColor: "hsl(0 100% 74%)",
-        borderWidth: "2px"
+        borderWidth: "2px",
     }
 
+
+    const errorFontColor = {
+        color: "hsl(0 100% 74%)",
+    }
 
 
     return (
         <div className="input-error-box">
             <div className="input-box" style={props.error ? errorStyles : {}}>
                 <input
+                    style={props.error ? errorFontColor : {}}
                     id={props.id}
                     type={props.type}
                     placeholder={props.placeholder}
